dc92f8cea8a2849839eb4034aa8849b7b6f46e51
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2021-05-16 11:53:55 +0200

    End test

--- code/cpp/GDsampler.cpp
+++ code/cpp/GDsampler.cpp
@@ -3,6 +3,9 @@
 #include "particle.h"
 #include "WaveFunctions/wavefunction.h"
 
+#include <iostream>
+using namespace std;
+
 void GDsampler::gdsampler(std::vector<class Particle*> particles, double localEnergy){
   m_system->getWaveFunction()->sample(particles, localEnergy);
   return;
